97f4c16a4eed5c8af28250ed0aa58a8437ddd80f
FlutLab.io <>
2021-04-24 08:17:03 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.pink,
         accentColor: Colors.amber,
         canvasColor: Color.fromRGBO(255, 254, 229, 1), // Background color of the entire App
-        // fontFamily: 'Raleway',
+        fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
             body1: TextStyle(
               color: Color.fromRGBO(20, 51, 51, 1),
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
             ),
             title: TextStyle(
               fontSize: 20,
-              fontFamily: 'RobotoCondensed',
+              //   fontFamily: 'RobotoCondensed',
               fontWeight: FontWeight.bold,
             )),
       ),
