920259f8fdd3f503af54b49fb2bbb855a40b0ac9
FlutLab.io <>
2021-04-24 08:31:42 +0000

    Autosave

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -34,7 +34,7 @@ class CategoryItem extends StatelessWidget {
         decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [
-              color.withOpacity(0.2),
+              color.withOpacity(0.5),
               color,
             ],
             begin: Alignment.topLeft,
