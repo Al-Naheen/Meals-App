c23416391c5dbc3ad071cdf429b875e5571ea2ba
FlutLab.io <>
2021-04-24 09:06:55 +0000

    Autosave

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -22,7 +22,7 @@ class CategoriesScreen extends StatelessWidget {
             )
             .toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 300,
+          maxCrossAxisExtent: 00,
           childAspectRatio: 3 / 2, // width/height
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
