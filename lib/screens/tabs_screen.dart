f96d4f70937c3a2fc96dbf66fb34fb64e0789ae5
FlutLab.io <>
2021-04-26 10:40:49 +0000

    Autosave

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -12,7 +12,7 @@ class _TabsScreenState extends State<TabsScreen> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3,
+      length: 4,
       initialIndex: 1, // default is 0
       child: Scaffold(
         appBar: AppBar(
