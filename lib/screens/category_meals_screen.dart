0e15678fddbeb00496cc8ec2e46f0bfa74701833
FlutLab.io <>
2021-05-02 11:39:44 +0000

    Autosave

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -34,7 +34,7 @@ class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
       final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
       categoryTitle = routeArgs['title'];
       final categoryId = routeArgs['id'];
-      displayedMeals = DUMMY_MEALS.where((meal) {
+      displayedMeals = widget.availableMeals.where((meal) {
         return meal.categories.contains(categoryId);
       }).toList();
       _loadedInitData = true;
